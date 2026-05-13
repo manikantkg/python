afd4e6dd27ec695b6801081577fccd5e274db752
manikantkg <manikantacoder007@gmail.com>
2026-05-13 11:41:49 +0530

    string methods

---
+++ python-main/day1/string.py
@@ -0,0 +1,18 @@
+# Do the following:
+
+# Print it in UPPERCASE
+# Print the length of the string
+# Replace "golang" with "python"
+# Print only the word "developer" using slicing
+
+role="golang developer"
+
+print(role.upper())
+print(len(role))
+print(role.capitalize())
+
+print(role[0])
+print(role[0:6])
+print(role.startswith("gol"))
+print(role.replace("golang", "python"))
+print(role[7:])
