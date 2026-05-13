24738dd12b951e3fd6be65c03cd01d24e1ea842c
manikantkg <manikantacoder007@gmail.com>
2026-05-13 11:59:00 +0530

    conditionals

---
+++ python-main/day1/conditionals.py
@@ -0,0 +1,27 @@
+age = 26
+
+if age >=18:
+    print("You are an adult")
+elif age>=13:
+    print("You are a teenager")
+else:
+    print("You are a child")
+
+
+# Quick Exercise:
+# Write a program that:
+
+# Creates a variable experience = 5
+# If experience is greater than 7 → print "Senior Developer"
+# If experience is between 3 and 7 → print "Mid Level Developer"
+# If experience is less than 3 → print "Junior Developer"
+
+
+experience=5
+
+if experience >= 7:
+    print("Senior Developer")
+elif experience >= 3:
+    print("Mid Developer")
+else :
+    print("Junior Developer")
