6cc69b82196dced500e05409bcb7f0b85c33106a
manikantkg <manikantacoder007@gmail.com>
2026-05-13 11:31:17 +0530

    variables

---
+++ python-main/day1/variables.py
@@ -0,0 +1,18 @@
+name="Manikanta"
+age=36
+job_doing=True
+weight=76
+
+print(f"Name:{name}, Age:{age}, Status:{job_doing}, Weight:{weight}")
+
+
+
+# Quick Exercise:
+# Create variables for:
+
+# Your name
+# Your years of experience
+# Whether you are currently employed (True/False)
+# Your hourly rate as a decimal
+
+# Then print all four variables in one print() statement.
